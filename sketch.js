138fc437a7ea3e5e183b30e061b74e435c19e00f
ariannaaaa <arianna.smaron@students.fhnw.ch>
2018-04-16 18:58:13 +0200

    add responsive sketch add

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+let cnv;
 function setup() {
-  // put setup code here
+	cnv= createCanvas(innerWidth, innerHeight);
+	cnv.parent('p5Sketch');
 }
+
 
 function draw() {
   // put drawing code here
+  background(51);
+
+}
+
+function windowResized(){
+resizeCanvas(innerWidth, innerHeight);
+
 }
